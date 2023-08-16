30fe247beac3ac811f2798db4162f3d839479c4d
Yong Ha Seo <tjdydgk4596@gmail.com>
2023-08-16 18:05:54 +0900

    23년 08월 16일 자료구조 수업내용 추가

--- STLExercise04/STLExercise04.h
+++ STLExercise04/STLExercise04.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <iostream>
+#include <array>
+#include <vector>
+
+void Ex3()
+{
+	std::array<int, 4> myArray{1, 2, 3, 4};
+
+	for (const auto& e : myArray)
+	{
+		std::cout << e << " ";
+	}
+}
